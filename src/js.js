ca4eb9c1e6fbffee138e01e5b829bd85a33602cf
Andrej1999 <malahow.andrej2014@yandex.ru>
2022-07-03 19:09:27 +0700

    prodac

--- src/js.js
+++ src/js.js
@@ -4,6 +4,11 @@ const headerBar = document.querySelector(".nav-mobile__menu");
 const mediaQuvery = window.matchMedia('(max-width: 768px)');
 const overhidenBar = document.querySelector(".overhiden-baar");
 const closeBar = document.querySelector(".nav-mobile__desktop-close");
+const openModal = document.querySelector(".modal-window");
+const openModalDesktop = document.querySelector(".modal-window-desktop");
+const modalWindow = document.querySelector(".modal-login__desktop");
+const closeModalDesktop = document.querySelector(".modal-login__desktop-close");
+
 
 
 let offset = 0;
@@ -24,5 +29,44 @@ closeBar.addEventListener('click', function(){
     overhidenBar.style.display = "none";
     document.querySelector(".body").style.overflow = "scroll"
 })
+
+openModal.addEventListener('click', function(){
+    modalWindow.style.display = "block";
+    document.querySelector(".body").style.overflow = "hidden"
+})
+
+openModalDesktop.addEventListener('click', function(){
+    modalWindow.style.display = "block";
+    document.querySelector(".body").style.overflow = "hidden"
+})
+closeModalDesktop.addEventListener('click', function(){
+    modalWindow.style.display = "none";
+    document.querySelector(".body").style.overflow = "scroll"
+})
+
+
+let a = 15;
+let b = 15; 
+
+for( var i = 1; i <= 10; i++ ){
+
+function foo (e){
+    setTimeout(()=>{
+    console.log(e)
+}, 10000)};
+}
+foo(i)
+
+for( var i = 1; i <= 10; i++ ){
+setTimeout((y)=>{
+    console.log(y)
+}, 1000)};
+
+console.log(a+b);
+
+
+
+
+
 
 
